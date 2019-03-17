03a64f94940c0b59b2119c2c7a1cca5e3a1c740d
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-03-16 22:46:44 -0400

    HTML, CSS, JS & images updated (completed page)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
 (() => {
 console.log('JavaScript file is linked.');
 	
-	let malek    = document.getElementById('malek'), // Malek's Image
-		cindy    = document.getElementById('cindy'), // Cindy's Image
-		malekBox = document.getElementById('boxM'),  // Malek Div
-		cindyBox = document.getElementById('boxC'),  // Cindy Div
+// 	let malek    = document.getElementById('malek'), // Malek's Image
+// 		cindy    = document.getElementById('cindy'), // Cindy's Image
+// 		malekBox = document.getElementById('boxM'),  // Malek Div
+// 		cindyBox = document.getElementById('boxC'),  // Cindy Div
+// 		  
+
+// 	function expandM(){
+// 		malek.classList.toggle('expand');
+// 		
+// 		console.log('Malek transformed.');
+// 	};
+
+// 	function expandC(){
+// 		cindy.classList.toggle('expand')
+// 		bioC.classList.toggle('gone')
+// 		console.log('Cindy transformed.');
+// 	};
+
+
+// malek.addEventListener('click', expandM);
+
+// cindy.addEventListener('click', expandC);
+
+
+	let malek    = document.getElementById('malekH2'),
+		cindy    = document.getElementById('cindyH2'),
 		bioM     = document.getElementById('bioM'),  // Malek Bio
-		bioC     = document.getElementById('bioC');  // Cindy Bio
+ 		bioC     = document.getElementById('bioC'),	 // Cindy Bio
+		image    = document.getElementById('aviBox'),
+		sources  = ['images/malek.jpg', 'images/cindy.jpg'],
+		sourceM  = 'images/malek.jpg';
+		sourceC  = 'images/cindy.jpg';
 
-	function expandM(){
-		malek.classList.toggle('expand');
-		bioM.classList.toggle('gone')
-		console.log('Malek transformed.');
-	};
+		function malekDisplay(){
+			console.log('Malek displayed.');
+			image.src = sourceM;
+			bioM.classList.add('show');
+			bioC.classList.remove('show');
+			// image.src = 'images/malek.jpg';
+		}
 
-	function expandC(){
-		cindy.classList.toggle('expand')
-		bioC.classList.toggle('gone')
-		console.log('Cindy transformed.');
-	};
+		function cindyDiplay(){
+			console.log('Cindy displayed.');
+			image.src = sourceC;
+			bioC.classList.add('show');
+			bioM.classList.remove('show');
+		}
 
 
-malek.addEventListener('click', expandM);
+		malek.addEventListener('click', malekDisplay);
 
-cindy.addEventListener('click', expandC);
+		cindy.addEventListener('click', cindyDiplay);
+
 
 })();
